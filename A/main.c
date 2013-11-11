bcc2fc0e826a3a004ab1f0eca92567d675e133c0
lijian <hustlijian@hust.edu.cn>
2013-11-11 13:17:14 +0800

    add A, and change the Makefile to compile exe file for both windows and linux/unix user, add the gitignore

--- A/main.c
+++ A/main.c
@@ -3,9 +3,15 @@
 
 int main(int argc, char* argv[])
 {
+    int T, N;
     freopen("input.txt", "r", stdin);
     double start, end;
     start = clock();
+    scanf("%d", &T);
+    while (T--) {
+        scanf("%d", &N);
+        printf("%d\n", N*N+N/2+1);
+    }
 
     end = clock();
     printf("%.3lf MS\n", (end-start)/1000);	
